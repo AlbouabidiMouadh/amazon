c1c7201e6a80e9b244074c1216f8c2a12cdfd475
AlbouabidiMouadh <95055135+AlbouabidiMouadh@users.noreply.github.com>
2023-02-23 16:45:12 +0100

    third commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,24 @@
 import "./App.css";
+import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">amazon</a>
-      </header>
-      <main>
-        list of products
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">amazon</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen/>}/>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+          
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
