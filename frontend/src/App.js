c299fdc7f5ea11981bdce737e357c6e25ba764a0
AlbouabidiMouadh <95055135+AlbouabidiMouadh@users.noreply.github.com>
2023-02-23 14:57:07 +0100

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazon</a>
+      </header>
+      <main>
+        list of products
+      </main>
+    </div>
+  );
+}
+
+export default App;
